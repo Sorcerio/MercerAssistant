b1ca859c859c0b1a7a1480d161eb839ec67edcd7
maximombro <maximombro@gmail.com>
2019-03-12 15:02:06 -0400

    Confirmed all functions from base install in its current state

--- mainControl.py
+++ mainControl.py
@@ -13,7 +13,7 @@ def main():
     # Learn test text
     # mercer.learnTextFile("learn/test.txt")
     # mercer.learnTextFile("learn/2049.txt")
-    # mercer.learnTextFile("learn/PrideAndPrejudice.txt")
+    mercer.learnTextFile("learn/PrideAndPrejudice.txt")
 
     # Learn from Reddit skimming
     # mercer.learnFromSubReddit(10,"story")
@@ -27,7 +27,7 @@ def main():
     # print(mercer.createSentence(5))
 
     # Write text to file
-    # mercer.writeTextToFile(7,7,"testText.txt")
+    mercer.writeTextToFile(7,7,"testText.txt")
 
     # Exit the dictionary
     mercer.exitMercer()
